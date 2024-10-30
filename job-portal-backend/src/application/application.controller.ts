b1f50d520dd6cfa8e049fb3a6f2693e3b8b60c59
mislamfullstackdev <mislam.pasona@gmail.com>
2024-10-30 17:39:04 +0900

    Added application dto and applyJob api

--- job-portal-backend/src/application/application.controller.ts
+++ job-portal-backend/src/application/application.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Post, Req, UseGuards } from '@nestjs/common';
+import { ApplicationService } from './application.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('application')
-export class ApplicationController {}
+export class ApplicationController {
+    constructor(private readonly applicationService: ApplicationService){}
+
+    @UseGuards(JwtAuthGuard)
+    @Post(':id')
+    async applyJob(@Req() req: any, @Param('id') jobId: string){
+        const userId = req.user.id;
+        const application = await this.applicationService.applyJob(userId, jobId)
+        return {
+            application,
+            succcess: true,
+            message: "Your Application submitted successfully"
+        }
+    }
+}
--- job-portal-backend/src/application/application.service.ts
+++ job-portal-backend/src/application/application.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class ApplicationService {}
+export class ApplicationService {
+    constructor( private readonly prismaService: PrismaService){}
+
+    async applyJob(applicantId: string, jobId: string){
+        if(!jobId){
+            throw new BadRequestException("Job is required")
+        }
+        const alreadyApplied = await this.prismaService.application.findFirst({
+            where:{applicantId, jobId}
+        });
+        if(alreadyApplied){
+            throw new BadRequestException("Already applied this job")
+        }
+        // find the job
+        const job = await this.prismaService.job.findUnique({
+            where:{ id: jobId}
+        })
+
+        if(!job){
+            throw new BadRequestException("job not found")
+        }
+
+        const newApplication = await this.prismaService.application.create({
+            data: { 
+                applicantId, 
+                jobId 
+            }
+        });
+        return newApplication;
+    } 
+}
